c8e544d120326edb2ca9ecb380ad0b178cdadcc9
Andrew Johnson <earyzhe@gmail.com>
2022-09-07 14:21:41 +0200

    Add the progress reward function

--- src/models/My-Third_model/reward_function.py
+++ src/models/My-Third_model/reward_function.py
@@ -51,7 +51,8 @@ def direction_reward(params: dict, weight=1):
     if direction_diff > 180:
         direction_diff = 360 - direction_diff
 
-    def calc_reward(x): return x * weight
+    def calc_reward(x):
+        return x * weight
 
     # Reward or Penalize based on the difference
     if direction_diff > 30:
@@ -69,6 +70,16 @@ def direction_reward(params: dict, weight=1):
 def speed_reward(params: dict, weight=1):
     max_speed = 4
     return params.get('speed', 0) / max_speed * weight
+
+
+def progress_reward(params: dict, weight=1):
+    steps = params.get('steps', 0)
+    max_progress = 100
+    if steps <= 5:
+        return 0  # ignore progress in the first 5 steps
+    else:
+        progress = params.get('progress', 0)
+        return progress / max_progress / steps * weight
 
 
 def reward_function(params: dict):
@@ -91,8 +102,8 @@ def reward_function(params: dict):
     reward = reduce(lambda a, b: a + b, [
         1e-3,  # Minimum reward
         direction_reward(params, 1),
-        speed_reward(params, 2)
-
+        speed_reward(params, 2),
+        progress_reward(params, 10)
     ])
 
     # Always return a float value
--- src/utils/get_progess.py
+++ src/utils/get_progess.py
@@ -1,5 +1,9 @@
 
 
 def progress_reward(params: dict):
-    progress = params.get('progress', 0)
-    previous
+    steps = params.get('steps', 0)
+    if steps <= 5:
+        return 1  # ignore progress in the first 5 steps
+    else:
+        progress = params.get('progress', 0)
+        return 10 * progress / steps
